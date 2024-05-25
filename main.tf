6384b83fb59eed67e20d8e7a231ee54ff6539d4d
prajaktag-20 <106527966+prajaktag-20@users.noreply.github.com>
2024-05-25 22:05:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+   backend "s3" {
+     bucket_name = "statefile-demo-bucket"
+     key = "terraform.tfstate"
+     region = "ap-south-1"
+   }
+}
+
 resource "aws_vpc" "test_vpc" {
 	cidr_block = "10.0.0.0/16"
 	tags = {
